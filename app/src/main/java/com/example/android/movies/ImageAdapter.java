ef32772239afb8cab6662e131da49c5c490cdaf9
Juan Carlos <juancoz@hotmail.com>
2016-03-16 20:13:30 -0500

    Add icon and colors

--- app/src/main/java/com/example/android/movies/ImageAdapter.java
+++ app/src/main/java/com/example/android/movies/ImageAdapter.java
@@ -33,12 +33,6 @@ public class ImageAdapter extends BaseAdapter {
     public View getView(int position, View convertView, ViewGroup parent) {
         ImageView imageView;
         if (convertView == null) {
-            // if it's not recycled, initialize some attributes
-//            imageView = new ImageView(mContext);
-//            imageView.setLayoutParams(new GridView.LayoutParams(85, 85));
-//            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-//            imageView.setPadding(8, 8, 8, 8);
-
             LayoutInflater inflater = (LayoutInflater) mContext
                     .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             convertView = inflater.inflate(R.layout.grid_item_forecast, parent, false);
