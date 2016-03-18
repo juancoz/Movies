11fb7605b079644dd235d52d4f51367916f4107f
Juan Carlos <juancoz@hotmail.com>
2016-03-17 20:16:20 -0500

    Add format to fragment_detail.xml

--- app/src/main/java/com/example/android/movies/DetailActivityFragment.java
+++ app/src/main/java/com/example/android/movies/DetailActivityFragment.java
@@ -1,12 +1,16 @@
 package com.example.android.movies;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -26,8 +30,36 @@ public class DetailActivityFragment extends Fragment {
         Intent intent = getActivity().getIntent();
         if (intent != null && intent.hasExtra(Intent.EXTRA_TEXT)) {
             String forecastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
-            ((TextView) rootView.findViewById(R.id.detail_text))
-                    .setText(forecastStr);
+
+            // Split the String
+            String[] splitFor = forecastStr.split(" -- ");
+
+            // Load the title string
+            ((TextView) rootView.findViewById(R.id.title_textview))
+                    .setText(splitFor[0]);
+
+            // Load the ImageView
+            final String BASE_URL = "http://image.tmdb.org/t/p/";
+            final String SIZE = "w185";
+            Uri builtUri = Uri.parse(BASE_URL).buildUpon()
+                    .appendPath(SIZE)
+                    .appendPath(splitFor[1].substring(1, splitFor[1].length()))
+                    .build();
+
+            ImageView imageView = (ImageView) rootView.findViewById(R.id.poster_imageview);
+            Glide.with(imageView.getContext()).load(builtUri).into(imageView);
+
+            // Load the overview text
+            ((TextView) rootView.findViewById(R.id.overview_textview))
+                    .setText(splitFor[2]);
+
+            // Load release text
+            ((TextView) rootView.findViewById(R.id.release_textview))
+                    .setText(splitFor[3]);
+
+            //Load vote text
+            ((TextView) rootView.findViewById(R.id.vote_textview))
+                    .setText(splitFor[4] + " / 10");
         }
 
         return rootView;
